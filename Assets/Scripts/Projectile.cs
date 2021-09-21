278ede766b0a7fa7ac1cf5271721d3e669ada2c9
Troy <troydexter5@gmail.com>
2021-09-21 19:44:47 -0400

    Chapter 4 - Destroying old objects

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
